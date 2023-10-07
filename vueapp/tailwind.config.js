54d6157f840fa07371c98ab0fd3539d9760c7337
Kninja99 <kcdamschen@yahoo.com>
2023-10-07 15:45:44 -0700

    color pallet decided

--- vueapp/tailwind.config.js
+++ vueapp/tailwind.config.js
@@ -2,6 +2,18 @@
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      black: '#010101',
+      polar: '#f4f9fd',
+      silver: '#afafaf',
+      gray: '#4f4f50',
+      blue: '#115bfb',
+      turquoise: '#3bedb7',
+      yellow: '#fce702',
+      purple: '#8154f0',
+      'blue-lagoon': '#027788',
+      mud: '#bb8652',
+    },
     extend: {},
   },
   plugins: [],
