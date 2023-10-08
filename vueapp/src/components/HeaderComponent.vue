08aa505ff56709299a3cb13ccadb083ca85b8e76
Kninja99 <kcdamschen@yahoo.com>
2023-10-08 14:36:44 -0700

    logo and header start
    * logo and header started
    * need to work on nav bar

--- vueapp/src/components/HeaderComponent.vue
+++ vueapp/src/components/HeaderComponent.vue
@@ -1,12 +1,16 @@
-<template>
-  <header>
-    <!-- Website Logo -->
-    <div></div>
-  </header>
-</template>
-
 <script>
 export default {
   name: 'HeaderComponent',
 };
 </script>
+
+<template>
+  <header class="py-4">
+    <!-- Website Logo -->
+    <div class="flex items-center mx-7">
+      <img src="../assets/logo.svg" alt="logo" />
+      <p class="ml-4 text-lg font-medium">Plate Pioneer</p>
+    </div>
+    <!-- NavBar (TODO) -->
+  </header>
+</template>
